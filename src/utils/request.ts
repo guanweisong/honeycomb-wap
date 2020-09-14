5bcaf2edaeb089f844ee00e08c572cfd3b209cae
guanweisong <307761682@qq.com>
2020-09-14 21:33:16 +0800

    还原网络配置

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: process.env.NODE_ENV !== 'development' ? 'http://127.0.0.1:7001' : 'https://api.guanweisong.com',
+  baseURL: process.env.NODE_ENV === 'development' ? 'http://127.0.0.1:7001' : 'https://api.guanweisong.com',
   withCredentials: true,
   timeout: 10000,
   headers: {},
