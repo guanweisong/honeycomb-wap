944ae38feef005c59ce51ce12da4f1b35c54b9c6
guanweisong <307761682@qq.com>
2020-09-29 23:09:47 +0800

    全局背景色处理

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import { AppProps } from 'next/app'
 // @ts-ignore
 import withGA from 'next-ga'
 import '@/node_modules/antd-mobile/dist/antd-mobile.less'
+import '@/src/assets/markdown.less'
 import './app.less'
 
 NProgress.configure({ showSpinner: false })
@@ -18,19 +19,5 @@ Router.events.on('routeChangeError', () => NProgress.done())
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />
 }
-
-// MyApp.getInitialProps = async () => {
-//   const props = {
-//     menu: [],
-//   }
-//
-//   const queryMenuResult = await MenuServer.indexMenu()
-//   if (queryMenuResult.status === 200 ) {
-//     props.menu = queryMenuResult.data.list
-//   }
-//   return {
-//     appProps: props
-//   }
-// }
 
 export default withGA("UA-158268354-2", Router)(MyApp);
