d6fdd6248cdf6117ec644ff71d93710079472ce8
guanweisong <307761682@qq.com>
2022-10-21 08:36:49 +0800

    feat: 细节调整

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -149,7 +149,7 @@ const Menu = (props: MenuProps) => {
       <div className="w-10 px-2 cursor-pointer lg:hidden" onClick={() => setVisible(!visible)}>
         {[1, 1, 1].map(() => (
           <div
-            className={classNames('h-0.5 my-1.5 bg-gray-400 transition-all', {
+            className={classNames('h-0.5 my-1.5 bg-gray-500 transition-all', {
               'first:translate-y-2 first:rotate-45 even:opacity-0 last:-translate-y-2 last:-rotate-45':
                 visible,
             })}
