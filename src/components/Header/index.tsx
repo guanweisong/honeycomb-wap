b29c7035c20695ee487d108342f7ded5910e3a33
guanweisong <307761682@qq.com>
2022-10-21 08:41:34 +0800

    feat: 细节调整

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -22,7 +22,7 @@ const Header = (props: HeaderProps) => {
       </Head>
       <div className="container relative box-border h-full flex justify-between items-center">
         <div className="h-full flex items-center">
-          <Link href={'/list/category'}>
+          <Link href={'/list/category'} scroll={false}>
             <a className="text-pink-500 text-xl lg:text-2xl ml-2">{setting.site_name}</a>
           </Link>
         </div>
