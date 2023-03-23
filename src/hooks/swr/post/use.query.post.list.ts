8ddea256ba77d21ed57ccbade6e62759cde5c834
稻草人 <guanweisong@163.com>
2023-03-23 16:06:25 +0800

    Update use.query.post.list.ts

--- src/hooks/swr/post/use.query.post.list.ts
+++ src/hooks/swr/post/use.query.post.list.ts
@@ -7,7 +7,7 @@ const useQueryPostList = (params: PostListQuery, initData: PostEntity[]) => {
   const { data, error, mutate, size, setSize } = useSWRInfinite(
     (index) => ['/posts', { ...params, page: index + 1 }],
     (_url, props) => PostServer.indexPostList(props as PostListQuery),
-    { fallbackData: [initData] },
+    { fallbackData: [initData], revalidateAll: true },
   );
 
   return {
