73920d212b48b1410fe102945dcc2373589fab26
guanweisong <307761682@qq.com>
2023-09-20 08:02:24 +0800

    feat: enable SSG

--- src/app/pages/[id]/page.tsx
+++ src/app/pages/[id]/page.tsx
@@ -54,6 +54,6 @@ export async function generateMetadata(props: GenerateMetadataProps) {
   };
 }
 
-// export async function generateStaticParams() {
-//   return [];
-// }
+export async function generateStaticParams() {
+  return [];
+}
