90d804b9141c09b5ae98f6fe31f55aa879de458a
guanweisong <307761682@qq.com>
2023-11-30 09:02:57 +0800

    Revert "feat: cache all images, fonts"
    This reverts commit c92668f9e858f843b6f7577a835d7bf995e5b032.

--- next.config.js
+++ next.config.js
@@ -22,22 +22,6 @@ const nextConfig = {
     // !! WARN !!
     ignoreBuildErrors: true,
   },
-  async headers() {
-    return [
-      {
-        //cache all images, fonts, etc. in the public folder
-        //Note: Next.js default is 'public, max-age=0' which cases many reloads on Safari!
-        //Note: we use version hashes and therefore can use immutable
-        source: '/:all*(svg|jpg|jpeg|png|gif|ico|webp|mp4|ttf|otf|woff|woff2)',
-        headers: [
-          {
-            key: 'cache-control',
-            value: 'public, max-age=31536000, immutable',
-          },
-        ],
-      },
-    ];
-  },
   images: {
     domains: ['honeycomb-1257715480.cos.ap-shanghai.myqcloud.com'],
     formats: ['image/avif', 'image/webp'],
