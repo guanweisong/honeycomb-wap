0f72a130c26ae90b910102e866af26e9feb7b6bc
guanweisong <307761682@qq.com>
2024-01-16 20:41:53 +0800

    feat: modify container max width

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,12 @@ module.exports = {
   theme: {
     container: {
       center: true,
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+      },
     },
     extend: {},
   },
